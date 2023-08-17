7ffaaab3ced181f9ec3dfe5c0bb950051b3e1e61
darshanfolane20 <103483359+darshanfolane20@users.noreply.github.com>
2023-08-17 18:01:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,27 +21,3 @@ CONNECTION_PARAMETERS = {
 # create session
 conn = Session.builder.configs(CONNECTION_PARAMETERS).create()
 
-
-def verify_code(verification_code):
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM EMP WHERE code = '{verification_code}' AND attended = FALSE")
-    attendee_data = cursor.fetchone()
-    if attendee_data:
-        cursor.execute(f"UPDATE EMP SET attended = TRUE WHERE code = '{verification_code}'")
-        conn.commit()
-        return True
-    else:
-        return False
-st.title('Event Attendance Verification')
-verification_code = st.text_input('Enter Verification Code:')
-if st.button('Verify'):
-    if verification_code:
-        if verify_code(verification_code):
-            st.success('Code verified successfully! Attendance count increased.')
-        else:
-            st.error('Invalid code or code already used.')
-# Display attendance count
-cursor = conn.cursor()
-cursor.execute("SELECT COUNT(*) FROM attendees WHERE attended = TRUE")
-attendance_count = cursor.fetchone()[0]
-st.write(f'Total Attended: {attendance_count}')
