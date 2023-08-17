e812ede6f8543890374c922c0f0eff1fd53d2d0b
darshanfolane20 <103483359+darshanfolane20@users.noreply.github.com>
2023-08-17 18:23:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import snowflake.connector
 from snowflake.connector.pandas_tools import write_pandas
 from snowflake.snowpark.session import Session
 import streamlit as st
+import pandas as pd
 
 # snowpark connection
 CONNECTION_PARAMETERS = {
@@ -22,26 +23,32 @@ CONNECTION_PARAMETERS = {
 session = Session.builder.configs(CONNECTION_PARAMETERS).create()
 
 
-def verify_code(verification_code):
-    cursor = session.cursor()
-    cursor.execute(f"SELECT * FROM EMP WHERE code = '{verification_code}' AND attended = FALSE")
-    attendee_data = cursor.fetchone()
-    if attendee_data:
-        cursor.execute(f"UPDATE EMP SET attended = TRUE WHERE code = '{verification_code}'")
-        session.commit()
+# Verify the code and mark attendance
+def verify_and_mark_attendance(verification_code, attendee_id):
+    attendees = session.read.table("EMP")
+    filtered_attendee = attendees.filter(attendees["code"] == verification_code).filter(attendees["attendee_id"] == attendee_id).filter(attendees["attended"] == False)
+    if not filtered_attendee.collect().empty():
+        attendees.write \
+            .overwrite() \
+            .filter(attendees["code"] == verification_code) \
+            .filter(attendees["EMP_ID"] == attendee_id) \
+            .set("attended", True)
         return True
     else:
         return False
+# Streamlit app
 st.title('Event Attendance Verification')
+attendee_id = st.text_input('Enter Your Attendee ID:')
 verification_code = st.text_input('Enter Verification Code:')
 if st.button('Verify'):
-    if verification_code:
-        if verify_code(verification_code):
-            st.success('Code verified successfully! Attendance count increased.')
+    if attendee_id and verification_code:
+        if verify_and_mark_attendance(verification_code, int(EMP_ID)):
+            st.success('Code verified successfully! You are marked as attended.')
         else:
             st.error('Invalid code or code already used.')
+# Note: For security, you should also display the attendee's name for confirmation.
 # Display attendance count
-cursor = session.cursor()
-cursor.execute("SELECT COUNT(*) FROM attendees WHERE attended = TRUE")
-attendance_count = cursor.fetchone()[0]
+attendees = session.read.table("ATTENDEES")
+attendance_count = attendees.filter(attendees["attended"] == True).count()
 st.write(f'Total Attended: {attendance_count}')
+
